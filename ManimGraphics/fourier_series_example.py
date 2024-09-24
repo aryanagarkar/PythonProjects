65bb7807df95298193c3bcb41b1eeef981f9a735
aryanagarkar <arya.nagarkar@gmail.com>
2024-09-24 11:35:54 -0700

    Finished Fourier Series Transform

--- ManimGraphics/fourier_series_example.py
+++ ManimGraphics/fourier_series_example.py
@@ -7,10 +7,6 @@ class SquareFunction(Scene):
             y_range = [-1.5, 1.5, 1],
             x_length = 2*TAU,
             axis_config = {"color": GREEN},
-            #x_axis_config = {
-             #   "numbers_to_include":np.arange(-10, 10.1, 2),
-              #  "numbers_with_elongated_ticks": np.arange(-10, 10.2, 2),
-            #},
             tips = True,
             )
 
@@ -30,17 +26,33 @@ class SquareFunction(Scene):
 			return (2/(i*np.pi))*(2)*np.sin(i*x*np.pi)
 
 		square_graph = axes.plot(squareWave, x_range = (-4.9, 4.9, 0.01), **{"discontinuities": [x for x in range(-5, 6)]})
-		values = [0]
-		values1 = [0]
+		values = []
+		values1 = []
+		
+		cumulative_terms = ["sin(x)"]
+		cumulative_text = Text(" + ".join(cumulative_terms), font_size=24).next_to(axes, UP, buff=0.2)
+
 		index = 1
+
 		for i in range(1, 31, 2):
 			values.append(axes.plot(lambda x: SINN(x, i), x_range = (-4.9, 4.9, 0.1)))
 			values1.append(axes.plot(lambda x: Sinn(x, i), x_range = (-4.9, 4.9, 0.1), color = BLUE))
 
-		self.add(axes, square_graph,  values[1])
-		self.play(ReplacementTransform(values[1], values1[1]))
+		self.add(axes, square_graph,  values[0])
+		self.play(ReplacementTransform(values[0], values1[0]))
+		self.add(cumulative_text)
 
-		for i in range(2, 15):
+		fixed_y_position_for_text = 2
+		
+		for i in range(1, 15):
 			self.play(Create(values[i]))
 			self.wait(1)
-			self.play(ReplacementTransform(values[i], values1[i]), ReplacementTransform(values1[i-1], values1[i]))
+			self.play(ReplacementTransform(values[i], values1[i]), ReplacementTransform(values1[i - 1], values1[i]))
+			
+			if i > 1:
+				new_term = f"sin({i * 2 - 1}x)"
+				cumulative_terms.append(new_term)
+
+			updated_cumulative_text = Text(" + ".join(cumulative_terms), font_size=24).next_to(axes, UP, buff=0)
+			self.play(Transform(cumulative_text, updated_cumulative_text))
+			cumulative_text = updated_cumulative_text
